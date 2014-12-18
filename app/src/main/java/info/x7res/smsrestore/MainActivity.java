c68fab6aab00eaedcc8d21cc4e75bc8879c2ab00
x7 <x7.0@outlook.com>
2014-12-18 18:30:08 +0800

    query sms db file from sdcard

--- app/src/main/java/info/x7res/smsrestore/MainActivity.java
+++ app/src/main/java/info/x7res/smsrestore/MainActivity.java
@@ -1,20 +1,52 @@
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) 2014 x7
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
 package info.x7res.smsrestore;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
  * Created by x7 on 12/18/14.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    public static final String TAG = "MainActivity";
+    protected Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button_restore_from_db);
+        button.setOnClickListener(this);
     }
 
 
@@ -38,5 +70,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_restore_from_db:
+                SdcardDbHelper helper = new SdcardDbHelper(this);
+                helper.test();
+                break;
+            default:
+                Log.w(TAG, "onClick default");
+        }
     }
 }
---
+++ app/src/main/java/info/x7res/smsrestore/SdcardDbHelper.java
@@ -0,0 +1,125 @@
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) 2014 x7
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+package info.x7res.smsrestore;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * open db on sdcard
+ * Created by x7 on 12/18/14.
+ */
+public class SdcardDbHelper extends SQLiteOpenHelper {
+    public static final String TAG = "SdcardDbHelper";
+    private static final String DATABASE_NAME = "mmssms.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public SdcardDbHelper(Context context) {
+        super(new SdcardDbContext(context), DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, "oldVersion " + oldVersion + " newVersion " + newVersion);
+    }
+
+    public Cursor queryAllSms() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query("sms", null, null, null, null, null, "date", null);
+        Log.i(TAG, "count " + cursor.getCount());
+        return cursor;
+    }
+
+    public void test() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.query("sms", null, null, null, null, null, "date", "10");
+        Log.i(TAG, "count " + c.getCount());
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            Log.i(TAG, "body " + c.getString(c.getColumnIndex("body")));
+        }
+        c.close();
+    }
+
+    /**
+     * open db file on sdcard
+     */
+    private static class SdcardDbContext extends ContextWrapper {
+        public SdcardDbContext(Context context) {
+            super(context);
+        }
+
+        @Override
+        public File getDatabasePath(String name) {
+            if (!android.os.Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                Log.w(TAG, "no sdcard");
+                return null;
+            }
+            String dbPath = android.os.Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + Config.getFilePath();
+            Log.i(TAG, dbPath);
+            File dbFile = new File(dbPath);
+            if (!dbFile.exists()) {
+                Log.w(TAG, "no file");
+                return null;
+            }
+            Log.d(TAG, "db open !");
+            return dbFile;
+        }
+
+        @Override
+        public SQLiteDatabase openOrCreateDatabase(String name, int mode,
+                                                   SQLiteDatabase.CursorFactory factory) {
+            return SQLiteDatabase.openOrCreateDatabase(getDatabasePath(name), factory);
+        }
+
+        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+        @Override
+        public SQLiteDatabase openOrCreateDatabase(String name, int mode,
+                                                   SQLiteDatabase.CursorFactory factory,
+                                                   DatabaseErrorHandler errorHandler) {
+            return SQLiteDatabase.openOrCreateDatabase(getDatabasePath(name), factory);
+        }
+
+    }
+}
